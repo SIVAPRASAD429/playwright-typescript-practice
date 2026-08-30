a6968f431982958e6bde8844151dfa611d5ea2b3
SIVAPRASAD429 <103380444+SIVAPRASAD429@users.noreply.github.com>
2026-08-30 22:46:26 +0530

    Comment out unused dropdown option tests
    Commented out unused selectOption tests and logging.

--- tests/multiselectdropdown.spec.ts
+++ tests/multiselectdropdown.spec.ts
@@ -5,14 +5,14 @@ await page.goto("https://testautomationpractice.blogspot.com/");
 //Select option from dropdown
 
 const colors:Locator = page.locator("#colors");
-/*
+
 await colors.selectOption(['Red','Blue','Green']);//By visible text
 await colors.selectOption(['red','blue','green']);//By Value
 await colors.selectOption([{label:'Red'},{label:'Blue'},{label:'Green'}]);//By Label
 await colors.selectOption([{index:1},{index:2},{index:3}]);//By Index
 expect(colors).toHaveCount(7);
 await page.waitForTimeout(3000);
-*/
+
 
 // Check available options in the dropdown. Map Method & Trim content
 const coloroptions:Locator = page.locator("#colors>option");
@@ -27,8 +27,8 @@ console.log(coloroptionarray[i]);
 }
 
 // or
-/*
+
 for(const option of coloroptionarray){
 console.log(coloroptionarray);
-}*/
+}
 })

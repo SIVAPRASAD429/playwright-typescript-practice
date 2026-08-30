60d71475b2f6b41b0b111a1ff1456a12f82cbe6b
SIVAPRASAD429 <103380444+SIVAPRASAD429@users.noreply.github.com>
2026-08-30 22:47:37 +0530

    Enable dynamic element tests in dynamicxpaths.spec.ts
    Uncomment dynamic element tests using Xpath and CSS locators.

--- tests/dynamicxpaths.spec.ts
+++ tests/dynamicxpaths.spec.ts
@@ -1,4 +1,4 @@
-/*import {test, expect, Locator} from "playwright/test";
+import {test, expect, Locator} from "playwright/test";
 test("DyamicElementsbyxpath", async ({page}) => 
     {
 await page.goto('https://testautomationpractice.blogspot.com/');
@@ -6,19 +6,19 @@ for( let i=1; i<=5; i++)
 {
     //Using Xpath
 
-    /*let button1:Locator = page.locator('//button[text() ="START" or text()="STOP"]');
+    let button1:Locator = page.locator('//button[text() ="START" or text()="STOP"]');
     await button1.click();
     await page.waitForTimeout(2000);
     await button1.click();*/
 
     //Using CSS Locator
-    /*const button1:Locator = page.locator('button[name = "start"], button[name = "stop"]');
+    const button1:Locator = page.locator('button[name = "start"], button[name = "stop"]');
     await button1.click();
     await page.waitForTimeout(2000);
     await button1.click();
-    */
+    
    //Using Playwright built-in Locator
-   /*
+   
    const button1:Locator = page.getByRole('button',{name: /START|STOP/});
     await button1.click();
     await page.waitForTimeout(2000);
@@ -26,5 +26,3 @@ for( let i=1; i<=5; i++)
 
 }
 });
-
-*/
